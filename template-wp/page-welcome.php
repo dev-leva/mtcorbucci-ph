53f693cb056d79000f78c8c339b53f9f90d186cd
andrea-buzz <54395135+andrea-buzz@users.noreply.github.com>
2020-05-27 13:24:12 +0200

    Add files via upload

---
+++ template-wp/page-welcome.php
@@ -0,0 +1,57 @@
+<?php
+/*
+* Template Name: Welcome
+* Description: Template for displaying a welcome page
+*/
+
+if( has_post_thumbnail() ){
+	$s_background = sprintf("style=\"background-image('%s')\" ", get_the_post_thumbnail_url(null, 'full'));
+	
+}else{
+	$s_background = '';
+}
+?><!doctype html>
+<html lang="it" <? echo $s_background ?>>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<meta name="description" content="Maria Teresa Corbucci PH.">
+<meta name="author" content="">
+<link rel="icon" href="../../favicon.ico">
+<link rel="canonical" href="<? the_permalink() ?>">
+
+<title>Maria Teresa Corbucci PH.</title>
+
+<link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
+<!-- Custom styles for this template -->
+<link 
+href="http://mariateresacorbucci.com/wp-content/themes/mauer-stills/cover.css" rel="stylesheet">
+
+</head>
+<body <? echo $s_background ?>>
+	<div class="wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="inner pad-allestimento pull-right col-md-6">
+					<h1 class="cover-heading text-center color-1">
+						Maria Teresa Corbucci
+					</h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="inner pull-right col-xs-10 col-sm-6 col-md-6">
+                                    <h2><a href="<? echo get_post_permalink(35) ?>" class="cover-heading"> &succ;&succ; </a></h2>
+				</div>
+			</div>
+		</div>
+	</div>
+	<a href="<? echo get_post_permalink(35) ?>" class="expanded"></a>
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="https://code.jquery.com/jquery-1.12.4.min.js"
+		integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ"
+		crossorigin="anonymous"></script>
+	<script> //window.jQuery || document.write('<script src="/js/jquery.min.js"><\/script>')</script>
+</body>
+</html>
